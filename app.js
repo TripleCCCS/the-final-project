59f79a2086855b590b983671452fe9ee86bda75d
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-05-21 10:11:09 -0400

    signup/login route working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./configs/passport-config');
 
 const bodyParser      = require('body-parser');
 const cookieParser    = require('cookie-parser');
@@ -61,67 +62,6 @@ app.use(session({
 
 // default value for title local
 app.locals.title = "Tripple C'z";
-
-//passport config area
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-passport.use(new LocalStrategy({
-  passReqToCallback: true
-}, (req, username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
-}));
-
-//google login config
-passport.use(new GoogleStrategy({
-  clientID: "462796486579-56ufkfhv279bospu4ne2jr1dvt218as4.apps.googleusercontent.com",
-  clientSecret: "_kpmbdhKU8TVB3RDPXtKObgy",
-  callbackURL: "/auth/google/callback"
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleID: profile.id }, (err, user) => {
-    if (err) {
-      return done(err);
-    }
-    if (user) {
-      return done(null, user);
-    }
-
-    const newUser = new User({
-      googleID: profile.id
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        return done(err);
-      }
-      done(null, newUser);
-    });
-  });
-
-}));
-// end passport config area
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
